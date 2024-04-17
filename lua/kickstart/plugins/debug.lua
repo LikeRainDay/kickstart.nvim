8f12d9327c38f129e519ffe39f0a64659be7689e
候帅 <houshuai0816@126.com>
2024-04-17 22:35:08 +0800

    fix: 调整命令快捷键

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -48,10 +48,10 @@ return {
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>ds', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<leader>de', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>dC', function()
+    vim.keymap.set('n', '<leader>dd', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
 
